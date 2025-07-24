7a5185fa3f181fa0f8da8dedeb81c2e0117bca01
Vikas Sharma <vikas.sharma24@ibm.com>
2025-07-24 12:06:00 +0530

    updated testcases and resolve comments

--- network_schema/network_schema.go
+++
@@ -1,9 +0,0 @@
-package network_schema
-
-import (
-	gen "github.com/ibm-hyper-protect/contract-go/common/general"
-)
-
-func HpcrVerifyNetworkConfig(network_config string) error {
-	return gen.VerifyNetworkSchema(network_config)
-}
