d3fc77d657ffacdf1cc2ff40a3acfb70a81792b4
Sashwat K <Sashwat.K@ibm.com>
2026-02-03 15:30:04 +0530

    chore: review comments

--- common/encrypt/encrypt_test.go
+++ common/encrypt/encrypt_test.go
@@ -321,16 +321,12 @@ func TestSignContract(t *testing.T) {
 func TestGenFinalSignedContract(t *testing.T) {
 	_, err := GenFinalSignedContract("test1", "test2", "test3", "")
 
-	if err != nil {
-		t.Errorf("failed to generate final signed and encrypted contract - %v", err)
-	}
+	assert.NoError(t, err, "failed to generate final signed and encrypted contract")
 }
 
 // Testcase to check if GenFinalSignedContract() is able to generate signed contract with attestation public key
 func TestGenFinalSignedContractAttest(t *testing.T) {
 	_, err := GenFinalSignedContract("test1", "test2", "test3", "test4")
 
-	if err != nil {
-		t.Errorf("failed to generate final signed and encrypted contract - %v", err)
-	}
+	assert.NoError(t, err, "failed to generate final signed and encrypted contract")
 }
