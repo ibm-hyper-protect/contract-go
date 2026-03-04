67ecca049d50642b2d0e07a29acff0a75a270f0d
Sashwat K <145958222+Sashwat-K@users.noreply.github.com>
2026-03-04 16:53:41 +0530

    feat: Add support REV 26 (#201)
    * feat: add REV 26 image as default cert
    
    * feat: update contract schema

--- schema/contract/schema.go
+++ schema/contract/schema.go
@@ -19,8 +19,5 @@ import (
 	_ "embed"
 )
 
-//go:embed hpse-contract-schema-1.0.93.json
-var ContractSchemaHpvs string
-
-//go:embed hpse-contract-schema-1.0.93-rhel.json
-var ContractSchemaHpcrRhvs string
+//go:embed hpse-contract-schema-1.0.101.json
+var ContractSchema string
