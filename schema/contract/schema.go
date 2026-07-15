aed8b33c7f810139731c615051c9ab2ea29d8cc3
Vikas Sharma <vikas.sharma24@ibm.com>
2026-07-15 11:45:01 +0530

    feat: add ccrt 2.2.4 encryption cert (#274)
    * feat: add ccrt 2.2.4 encryption cert
    
    * feat: updated ccrt schema

--- schema/contract/schema.go
+++ schema/contract/schema.go
@@ -19,7 +19,7 @@ import (
 	_ "embed"
 )
 
-//go:embed hpse-contract-schema-1.0.112.json
+//go:embed hpse-contract-schema-1.0.116.json
 var ContractSchemaCcrt string
 
 //go:embed hpse-contract-schema-1.0.112-rhel.json
